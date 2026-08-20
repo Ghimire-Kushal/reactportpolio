5a5e596c624aa703ed2937662f8a091cf1c544ea
Kushal Ghimire <kushal.upr@gmail.com>
2026-08-20 13:14:26 +0545

    push the fix the change

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -8,7 +8,7 @@ from sqlalchemy import select
 from app.config import settings
 from app.database import get_db
 from app.models.user import User
-
+# change
 bearer = HTTPBearer()
 
 def verify_password(plain: str, hashed: str) -> bool:
